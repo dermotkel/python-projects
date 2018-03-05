d942be5487dc90b9902e9460984936f7fbbd1534
dermotkel <35706109+dermotkel@users.noreply.github.com>
2018-03-05 17:28:23 +0000

    Update fact.py

--- fact.py
+++ fact.py
@@ -2,10 +2,9 @@
 # This program creates a function which outputs the factorial of a given number. 
 # 04/03/2018
 def factorial(x):
-    y = 1 #The first number is multiplied by one to give itself.
-    for i in range(x, 1, -1): #The for loop goes from the given number down to zero
-        y = y*i # Each number is multiplied multipled by the previous answer until the for loop reachs 1.
-    return y
+    for i in range(x-1, 1, -1): #The for loop goes from inputed number minus one down to zero
+        x = x*i # Each number is multiplied multipled by the previous answer until the for loop reachs one.
+    return x
 
 print("The factorial of 5 is", factorial(5))
 print("The factorial of 7 is", factorial(7))
